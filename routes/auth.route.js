7056d3f53c1cc3b994bc80d992cd82d30557ad5d
VadSmith <oldfatham@gmail.com>
2022-10-10 20:25:42 +0700

    add login, registration routes

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,9 +1,20 @@
 const { Router } = require('express');
 const router = Router();
+const User = require('../models/User');
 
 router.post('/registration', async (req, res) => {
   try {
     const { email, password } = req.body;
+    const isUsed = await User.findOne({ email })
+
+    if (isUsed) {
+      return res.status(300).json({ message: 'Данный Email уже занят' })
+    }
+
+    const user = new User({ email, password })
+    await user.save()
+    res.status(201).json({ message: 'Пользователь создан' })
+
   } catch (error) {
     console.error(error);
   }
